1ac12dce6fe2d195378394cc6e721788afe14e04
S0KL0-0 <alekss.semjonovs.work@gmail.com>
2025-06-02 13:15:08 +0300

    .

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+const task_mockup = {
+    title: '1. Jautājums',
+    question: 'Kā ietriekties kokā?',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Car_crash_1.jpg/1024px-Car_crash_1.jpg',
+    type: 'single', // single / multi
+    options: ['Ietriekties kokā',
+        'Braukt pa ceļu',
+        'ietriekties mājā'],
+    answer: 'Ietriekties kokā'
+}
+
+const tasks = [{}, {}, {}];
+
