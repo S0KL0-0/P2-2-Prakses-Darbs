fa54569d9551c9f1234e7e8bc3a3f73a3ccdd0d3
S0KL0-0 <alekss.semjonovs.work@gmail.com>
2025-06-02 13:40:45 +0300

    .

--- scripts.js
+++ scripts.js
@@ -1,14 +1,52 @@
 
-const task_mockup = {
-    title: '1. Jautājums',
-    question: 'Kā ietriekties kokā?',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Car_crash_1.jpg/1024px-Car_crash_1.jpg',
-    type: 'single', // single / multi
-    options: ['Ietriekties kokā',
-        'Braukt pa ceļu',
-        'ietriekties mājā'],
-    answer: 'Ietriekties kokā'
+const tasks = [
+    {
+        name: '1. Jautājums',
+        question: 'Kā ietriekties kokā?',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Car_crash_1.jpg/1024px-Car_crash_1.jpg',
+        type: 'single', // single / multi
+        options: ['Ietriekties kokā',
+            'Braukt pa ceļu',
+            'ietriekties mājā'],
+        answer: 'Ietriekties kokā'
+    }
+];
+const task_count = 1;
+
+selected_tasks = [];
+
+function get_new_task() {
+    const index = 0;
+
+    selected_tasks.push(index);
+
+    return tasks[index];
 }
 
-const tasks = [{}, {}, {}];
+function set_new_task(task) {
 
+    console.log(task);
+
+    const Start_Button = document.getElementById("start-button");
+    Start_Button.style.display = 'none';
+
+    const Title = document.getElementById("name");
+    Title.textContent = task.name;
+
+    console.log(Title);
+
+    const Question = document.getElementById("question");
+    Question.textContent = task.question;
+
+    const Image = document.getElementById("image");
+    Image.src = task.image;
+
+   // const Options = document.getElementById("options");
+   // Options.innertext = task.options;
+
+}
+
+function startTest() {
+    const task = get_new_task();
+    set_new_task(task);
+}
